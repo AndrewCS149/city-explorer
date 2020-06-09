09805540bc1ba1f86e6ede565db4a03377cbc58f
AndrewCS149 <andrewcs149@gmail.com>
2020-06-08 19:22:14 -0700

    Grab city data.

--- server.js
+++ server.js
@@ -4,22 +4,37 @@ const app = express()
 require('dotenv').config();
 const PORT = process.env.PORT || 3001;
 
-// Test run
-app.get('/', function (req, res) {
-  res.send('Hello World')
-});
-
 // remove browser protection to be able to pull data
-const cors = require('cors');
-app.use(cors());
+// this throws an error for some reason
+// const cors = require('cors');
+// app.use(cors());
+
+// Test run
+// app.get('/', function (req, res) {
+//   res.send('Hello World');
+// });
 
 // location path
 app.get('/location', (req, res) => {
 
-  let jsonData = req('data/location.json');
-  let searchQuery = req.query.city;
-  let wxData = new Location(searchQuery, jsonData[0])
+  try {
+    // console.log(req.query.city)
+    let searchQuery = req.query.city;
+    let jsonData = req('data/location.json');
+    let wxData = new Location(searchQuery, jsonData[0]);
+
+    console.log(wxData);
+
+    res.status(200).send(wxData);
+  } catch (err) {
+    console.log("Error", err);
+    res.status(500).send("There was an error on our part.");
+  }
 });
+
+// app.get('/location', (req, res) => {
+//   res.send("HELLO ANDREW.");
+// })
 
 
 function Location(searchQuery, obj) {
