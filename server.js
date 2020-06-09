31f2db1b055ed3551efa0b3f8395b12097b26fe7
AndrewCS149 <andrewcs149@gmail.com>
2020-06-08 21:03:36 -0700

    Install cors package.

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 3001;
 
 // remove browser protection to be able to pull data
 // this throws an error for some reason
-// const cors = require('cors');
-// app.use(cors());
+const cors = require('cors');
+app.use(cors());
 
 // Test run
 // app.get('/', function (req, res) {
@@ -20,7 +20,7 @@ app.get('/location', (req, res) => {
   try {
     // console.log(req.query.city)
     let searchQuery = req.query.city;
-    let jsonData = req('data/location.json');
+    let jsonData = require('./data/location.json');
     let wxData = new Location(searchQuery, jsonData[0]);
 
     console.log(wxData);
@@ -32,19 +32,12 @@ app.get('/location', (req, res) => {
   }
 });
 
-// app.get('/location', (req, res) => {
-//   res.send("HELLO ANDREW.");
-// })
-
-
 function Location(searchQuery, obj) {
   this.searchQuery = searchQuery;
   this.formatQuery = obj.formatted_query;
   this.lat = obj.latitude;
   this.long = obj.longitude;
 }
-
-
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
